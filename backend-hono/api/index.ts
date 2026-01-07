36fbc3d1884894cb452dc106386a8e2985527417
Fadilah Riczky <friczky@gmail.com>
2026-01-07 16:24:52 +0700

    config: Add `api.bodyParser: false` to Vercel configuration.

--- backend-hono/api/index.ts
+++ backend-hono/api/index.ts
@@ -3,7 +3,10 @@ import { handle } from '@hono/node-server/vercel'
 import { app } from '../src/index'
 
 export const config = {
-    runtime: 'nodejs'
+    runtime: 'nodejs',
+    api: {
+        bodyParser: false,
+    },
 }
 
 export default handle(app)
